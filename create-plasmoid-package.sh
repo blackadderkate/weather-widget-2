4553f21998d50f1a49bfe00956aa6b73329d6eea
Kate <blackadderkate@gmail.com>
2022-01-16 19:39:04 +0000

    Modify Widget build script

--- create-plasmoid-package.sh
+++ create-plasmoid-package.sh
@@ -2,10 +2,10 @@
 
 DIR="$( dirname "${BASH_SOURCE[0]}" )"
 cd $DIR
-rm -r build
-mkdir build
+rm -r buildWidget
+mkdir buildWidget
 
 cd package
 
-zip  -r ../build/weather-widget-2.plasmoid * --exclude \.git\*
+zip  -r ../buildWidget/weather-widget-2.1.plasmoid * --exclude \.git\*
 
