fc8a425139a83155df52f1ac2966321270a44c48
Kate <blackadderkate@gmail.com>
2022-01-20 19:44:00 +0000

    Amend Build Script

--- create-plasmoid-package.sh
+++ create-plasmoid-package.sh
@@ -2,10 +2,10 @@
 
 DIR="$( dirname "${BASH_SOURCE[0]}" )"
 cd $DIR
-rm -r build
-mkdir build
+rm -r buildWidget
+mkdir buildWidget
 
 cd package
 
-zip  -r ../build/weather-widget-2.plasmoid * --exclude \.git\* contents/code/db/meh.sh contents/code/db/
+zip  -r ../buildWidget/weather-widget-2.plasmoid * --exclude \.git\* contents/code/db/meh.sh contents/code/db/
 
