f3db845b45fbc3979534fbac5760f8537b662eba
Kate <blackadderkate@gmail.com>
2022-01-26 17:56:38 +0000

    Added script to increment Build Number

--- incBuild.sh
+++ incBuild.sh
@@ -5,6 +5,8 @@ IFS="." read -a TMP <<< $VERSION
 BUILD=$((${TMP[2]} + 1))
 # echo $(($BUILD + 1))
 NEWVERSION="${TMP[0]}\\.${TMP[1]}\\.$BUILD"
-sed -i "s/$VERSION/$NEWVERSION/" "./package/metadata.desktop"
-sed -i "/Plasmoid\ version/ s/$VERSION/$NEWVERSION/" "./package/contents/ui/config/ConfigGeneral.qml"
+CURRDIR=$PWD
+echo $CURRDIR
+sed -i "s/$VERSION/$NEWVERSION/" "$CURRDIR/package/metadata.desktop"
+sed -i "/Plasmoid\ version/ s/$VERSION/$NEWVERSION/" "$CURRDIR/package/contents/ui/config/ConfigGeneral.qml"
 echo "Updated Version number from $VERSION to ${TMP[0]}.${TMP[1]}.$BUILD"
