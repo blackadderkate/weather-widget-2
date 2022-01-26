e35769a049d5134a528e1e037cf3f76447e79555
Kate <blackadderkate@gmail.com>
2022-01-26 19:34:27 +0000

    Tweak create plasmoid script

--- create-plasmoid-package.sh
+++ create-plasmoid-package.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 DIR="$( dirname "${BASH_SOURCE[0]}" )"
-VERSION="$( grep -i PluginInfo-Version package/metadata.desktop | cut -d'=' -f 2 )"
+CURRDIR=$PWD
+VERSION="$( grep -i PluginInfo-Version $CURRDIR/package/metadata.desktop | cut -d'=' -f 2 )"
 echo $VERSION
 cd $DIR
 rm -r buildWidget
