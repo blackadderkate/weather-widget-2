bae5e953ac9cb922b534a46192f19d6d1007a1ca
Kate <blackadderkate@gmail.com>
2022-02-08 21:34:31 +0000

    Update Widget Name in build files

--- plugin/plasmoidplugin.cpp
+++ plugin/plasmoidplugin.cpp
@@ -6,7 +6,7 @@
 
 void PlasmoidPlugin::registerTypes(const char *uri)
 {
-    Q_ASSERT(uri == QLatin1String("org.kde.private.weatherWidget"));
+    Q_ASSERT(uri == QLatin1String("org.kde.private.weatherWidget-2"));
     
     qmlRegisterType<Backend>(uri, 1, 0, "Backend");
 }
