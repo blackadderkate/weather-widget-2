eef1c98102d3cc95070aa9717665725313c22d68
Kate <blackadderkate@gmail.com>
2022-02-09 00:51:34 +0000

    Don't install widget translations
    Fix issue #38:
    Need to exclude locale and translations directories from "Make Install".
    
    Widget-only locale/ files should not be installed into
    /usr/share/plasma/plasmoids/
    
    Easiest fix is to move the Locale directory to /tmp while we compile...

--- dev-build.sh
+++ dev-build.sh
@@ -9,8 +9,13 @@ fi
 
 sudo rm -fr build/*
 
-cd build
+TMP=$(mktemp -d -u)
+mv package/contents/locale $TMP
+pushd  ./build
 
 cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_LIBDIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
 make
 sudo make install
+
+popd
+mv $TMP package/contents/locale
