19d656e8fed693654a3476c49a57a82314620b9f
Mike <RedSQL@users.noreply.github.com>
2024-03-10 18:29:50 +0000

    Check properly if command failed

--- dev-build.sh
+++ dev-build.sh
@@ -18,7 +18,7 @@ pushd  ./build
 
 cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_LIBDIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON -DQT_MAJOR_VERSION=6
 make
-if [ -z "$?" ]; then
+if [ $? -eq 0 ]; then
   sudo make install
 else
   echo "ERROR: One of the CMake commands failed. Installation will not proceed."
